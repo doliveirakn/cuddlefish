64cb7f414e7cb3077cadd199927c62ea5e6fd6cf
Kyle d'Oliveira <kyle.doliveira@clio.com>
2017-12-01 15:18:07 -0800

    remove the class level method monkey patches on the migrator. Instead when we execute a migration in a transaction apply the cuddlefish changes there

--- lib/cuddlefish/migrations.rb
+++ lib/cuddlefish/migrations.rb
@@ -18,60 +18,20 @@ module ActiveRecord
   end
 
   class Migrator
-    class << self
+    # This is a monkey-patch. The previous version (in 4.2.8) was:
+    #
+    # def execute_migration_in_transaction(migration, direction)
+    #   ddl_transaction(migration) do
+    #     migration.migrate(direction)
+    #     record_version_state_after_migrating(migration.version)
+    #   end
+    # end
 
-      # This is a monkey-patch. The previous version (in 4.2.8) was:
-      #
-      # def run(direction, migrations_paths, target_version)
-      #   new(direction, migrations(migrations_paths), target_version).run
-      # end
-
-      def run(direction, migrations_paths, target_version)
-        relevant_migrations = migrations(migrations_paths).select {|m| m.version == target_version }
-        relevant_migrations.each do |migration|
-          run_migration_on_shard(direction, migration, target_version)
-        end
-      end
-
-      # This is a monkey-patch. The previous version (in 4.2.8) was:
-      #
-      # def up(migrations_paths, target_version = nil)
-      #   migrations = migrations(migrations_paths)
-      #   migrations.select! { |m| yield m } if block_given?
-      #   new(:up, migrations, target_version).migrate
-      # end
-
-      def up(migrations_paths, target_version = nil)
-        migrations = migrations(migrations_paths)
-        migrations.select! { |m| yield m } if block_given?
-
-        migrations.each do |migration|
-          run_migration_on_shard(:up, migration, target_version)
-        end
-      end
-
-      # This is a monkey-patch. The previous version (in 4.2.8) was:
-      #
-      # def down(migrations_paths, target_version = nil)
-      #   migrations = migrations(migrations_paths)
-      #   migrations.select! { |m| yield m } if block_given?
-      #   new(:down, migrations, target_version).migrate
-      # end
-
-      def down(migrations_paths, target_version = nil)
-        migrations = migrations(migrations_paths)
-        migrations.select! { |m| yield m } if block_given?
-
-        migrations.each do |migration|
-          run_migration_on_shard(:down, migration, target_version)
-        end
-      end
-
-      private
-
-      def run_migration_on_shard(direction, migration, target_version)
-        Cuddlefish.force_shard_tags(*migration.tags) do
-          new(direction, [migration], target_version).migrate
+    def execute_migration_in_transaction(migration, direction)
+      Cuddlefish.force_shard_tags(*migration.tags) do
+        ddl_transaction(migration) do
+          migration.migrate(direction)
+          record_version_state_after_migrating(migration.version)
         end
       end
     end
